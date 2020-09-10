7e79a058d11e5701c856c0e380523a09fdebefa6
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2020-09-10 20:50:35 -0300

    Fixes Typescript compiling

--- assets/ts/src/App.tsx
+++ assets/ts/src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 const AppComponent = () => {
-  return <h1>Welcome to React</h1>;
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+    </div>
+  );
 };
 
 export const App = hot(module)(AppComponent);
