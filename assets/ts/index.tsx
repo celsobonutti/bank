4802c6684124b8815d1593d3d98d9be2c978f841
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2020-09-10 21:38:13 -0300

    Compiles CSS separately from JS

--- assets/ts/index.tsx
+++ assets/ts/index.tsx
@@ -2,8 +2,6 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import '../css/app.scss';
-
 import { App } from './src/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- lib/bank_web/endpoint.ex
+++ lib/bank_web/endpoint.ex
@@ -23,8 +23,8 @@ defmodule BankWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :bank,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    gzip: true,
+    only: ~w(ts css fonts images favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
